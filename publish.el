039498ca25a7c002502b3be85b4b60f81cc6e80f
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-29 11:58:53 -0400

    re-add publish.el

---
+++ publish.el
@@ -0,0 +1,26 @@
+(require 'ox-publish)
+
+(let* ((base-directory ".")
+       (org-export-with-broken-links t)
+       (org-publish-project-alist `(("html"
+				     :base-directory ,base-directory
+				     :base-extension "org"
+				     :publishing-directory ,(concat base-directory "docs")
+				     :exclude "docs"
+				     :recursive t
+				     :publishing-function org-html-publish-to-html
+				     :auto-preamble t
+				     :auto-sitemap t)
+				    
+				    ("static-html"
+				     :base-directory ,base-directory
+				     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|dat\\|mov\\|txt\\|svg\\|aiff"
+				     :publishing-directory ,(concat base-directory "docs")
+				     :exclude "docs"
+				     :recursive t
+				     :publishing-function org-publish-attachment)
+
+				    ;; ... all the components ...
+				    ("scimax-eln" :components ("html" "static-html")))))
+
+  (org-publish "scimax-eln" t))
