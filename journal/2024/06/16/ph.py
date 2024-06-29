d99de37fa40dffb4b934aadc3e06c324acf9edf4
jkitchin <jkitchin@users.noreply.github.com>
2024-06-29 15:11:58 +0000

    deploy: 2dd5f8f53b473afc73ecf587c327cd8e3778792c

--- journal/2024/06/16/ph.py
+++
@@ -1,9 +0,0 @@
-from pycse.hashcache import hashcache
-import nava
-
-@hashcache(verbose=True)
-def f(a, b=1):
-    nava.play('../../../../cash-register-fake-88639.mp3')
-    return (a, b)
-
-print(f(1))
